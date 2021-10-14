ae0c09fb004b980519a424399294ec2ff782cf35
Ubuntu <ubuntu@ip-172-31-18-29.us-east-2.compute.internal>
2021-10-14 02:04:28 +0000

    made changes based on style guide

--- customers.sql
+++ customers.sql
@@ -1,4 +1,10 @@
-select c.id, c.name, c.email, min(created_at) as first_order_at, count(*) as number_of_orders from `analytics-engineers-club.coffee_shop.customers` c 
+select c.id,
+c.name,
+c.email,
+min(created_at) as first_order_at, 
+count(*) as number_of_orders
+from `analytics-engineers-club.coffee_shop.customers` c 
 left join `analytics-engineers-club.coffee_shop.orders` o on c.id = o.customer_id 
 group by c.id, c.name, c.email
-order by first_order_at limit 5
+order by first_order_at
+limit 5
